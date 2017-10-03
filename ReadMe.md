234ed010446adde107187491a779474afdf037dd
yeropharaoh <yeroschutte@gmail.com>
2017-10-03 02:44:13 +0200

    code cleanup

---
+++ ReadMe.md
@@ -0,0 +1,52 @@
+in a block, answer the following questions:
+- what is the problem you are trying to solve?
+•	People can find it difficult to plan ahead for vacations
+
+- who is your target audience?
+•	Families, students and couples
+
+- what are your specific goals?
+•	To make life easier for the vacationers by creating a packing list for 3 specific types of vacations.
+
+In another block, answer the following:
+- what is your business model? Where is your revenue coming from? 
+•	We plan to make this site into a mobile app costing 2.99 as a one time fee
+
+- what are the costs of your business?
+•	We just have to pay for the server (around 500) and mobile store fee
+
+Market research:
+- who is your current competition?
+•	There is currently no competition in form of a website. The pack point mobile app comes close but does not specialize in specific types of packing lists.
+
+- how is your product different from currently available competitors?
+•	We will be cheaper, and we’re going to offer a unique service: instead of focussing on activities while on vacation we will provide excellent management prior to your vacation
+
+- what is the current supply / demand for your product?
+•	a lot of demand, 365 days a year, because everybody can go on vacation whenever they please.
+
+
+technical specifications:
+- what data will you need to store? how will it be organized? describe each table, its columns, and its relationships with other tables.
+•	user information, packing list information. 
+•	it will be organized by user category: beach vacation, ski vacation, city trips.
+•	user: name, email, password
+•	packing list: items
+
+- what does your product look like? list each view, their purpose, and how they work
+•	home page + login, category page, list page (sunlist, skilist, citylist), profile page.
+
+have a timeline of milestones to reach, including deadlines:
+- create milestones that represent the discrete blocks of functionality.
+- give an estimate for how long each will take per engineer.
+- determine whether things can be built concurrently.
+- come up with a timeline of goals to stick to.
+
+1.	setting up github
+2.	setting up folder structure and file structure
+3.	creating a home, login and register page
+4.	creating a user
+5.	creating a profile page
+6.	creating the catogories
+7.	plus the to do’s
+8.	adding the styling
---
+++ src/app.js
@@ -0,0 +1,219 @@
+// REQURING NECCESSARIES
+const bcrypt = require('bcrypt');
+const Sequelize = require('sequelize');
+const express = require('express');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+
+let sequelize = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5000/group_proj`);
+
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + "/../public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: "whatever",
+    saveUninitialized: true,
+    resave: true
+}));
+
+const User = sequelize.define('users', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING
+    }
+}, {
+    timestamps: false
+});
+
+
+const PackingList = sequelize.define('packing_list', {
+    items: {
+        type: Sequelize.ARRAY(Sequelize.TEXT)
+    },
+}, {
+    timestamps: false
+});
+
+
+//Establishing relationships between models
+User.hasOne(PackingList);
+PackingList.belongsTo(User);
+
+// HOMEPAGE
+app.get('/', function(req, res) {
+    res.render('index', {
+        message: req.query.message,
+        user: req.session.user
+    });
+});
+
+// REGISTER
+app.post('/register', (req, res) => {
+    var password = req.body.rgpassword
+    bcrypt.hash(password, 8, (err, hash) => { //created hash for password security
+        if (err) throw err;
+        User.create({
+                username: req.body.rgusername,
+                email: req.body.rgemail,
+                password: hash
+            })
+            .then((user) => {
+                console.log("User create promise returned success!")
+                req.session.user = user;
+                res.redirect('/categories');
+                console.log('do i make it here?')
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    });
+
+});
+
+// LOGIN
+app.get('/login', function(req, res) {
+    res.render('login', { user: req.session.user })
+});
+
+app.post('/login', function(request, response) {
+    if (request.body.email.length === 0) {
+        response.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
+        return;
+    }
+    if (request.body.password.length === 0) {
+        response.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
+        return;
+    }
+    var email = request.body.email
+    var password = request.body.password
+
+    console.log(password);
+
+    User.findOne({
+            where: {
+                email: email
+            }
+        })
+        .then((user) => {
+            if (!user) {
+                response.redirect('/?message=' + encodeURIComponent("User doesn't exist."));
+            } else {
+                bcrypt.compare(password, user.password, (err, res) => { //validates password
+                    // console.log('Entered hashed password'+ password)
+                    // console.log('Database password'+user.password);
+                    if (res) {
+                        request.session.user = user;
+                        response.redirect('/categories');
+                    } else {
+                        response.redirect('/?message=' + encodeURIComponent("Incorrect password."));
+                    }
+                })
+            }
+        });
+});
+
+// CATEGORIES
+app.get('/categories', (req, res) => {
+    var user = req.session.user;
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("please log in to view your profile"))
+    } else {
+        res.render('categories', {
+            user: user
+        });
+    }
+});
+
+// BEACH PACKING LIST
+app.get('/sunlist', (req, res) => {
+    var user = req.session.user;
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("please log in to view your profile"))
+    } else {
+        res.render('sunlist', {
+            user: user
+        });
+    }
+});
+
+// BEACH PACKING LIST POST
+app.post('/sunlist', (req, res) => {
+    var forminfo = req.body
+    var user = req.session.user;
+
+    var emptyArray1 = []
+    var emptyArray2 = []
+
+    for (var X in forminfo) {
+        if (!forminfo.hasOwnProperty(X)) {
+            continue;
+        }
+        emptyArray1.push(X);
+        emptyArray2.push(forminfo[X]);
+    }
+
+    console.log(emptyArray1);
+    console.log(emptyArray2);
+    console.log(forminfo)
+
+    PackingList.create({
+            items: emptyArray2,
+            userId: user.id
+        })
+        .then(packinglist => {
+            var packinglist = packinglist;
+            res.render('/profile', { packinglist: packinglist })
+        })
+    res.redirect('sunlist');
+});
+
+// PROFILE
+app.get('/profile', (req, res) => {
+    var user = req.session.user;
+    if (user) {
+        User.findOne({
+                where: {
+                    id: user.id
+                }
+            })
+            .then((userfound) => {
+                PackingList.findOne({
+                        where: {
+                            userId: user.id
+                        }
+                    })
+                    .then(packinglist => {
+                        res.render('profile', { user: userfound, packinglist: packinglist })
+                    })
+            })
+    } else {
+        res.redirect('/?message=' + encodeURIComponent("Please login to view your profile."));
+    }
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+    req.session.destroy((error) => {
+        if (error) {
+            throw error;
+        }
+        res.redirect('/?message=' + encodeURIComponent("succesfully logged out"));
+    });
+});
+
+sequelize.sync({ force: false });
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000')
+})
